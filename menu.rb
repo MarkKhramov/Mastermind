f7c0e168ea979ee02ed8b30fa6f50c6279de5f1f
Mark Khramov <markkhramov@outlook.com>
2020-05-26 15:39:32 -0600

    Added instruction menu

--- menu.rb
+++ menu.rb
@@ -1,6 +1,6 @@
 class Menu
 
-  def main_menu()
+  def main_menu
     `clear`
     valid_command = false 
 
@@ -15,7 +15,34 @@ class Menu
     unless valid_command do
       input = gets
       
-      if gets
+      if gets == "P"
+        
+      elsif gets == "I"
+        print_instructions
+      end
     end 
+  end
+  end
+  
+  def print_instructions
+    `clear`
+
+    puts "Instructions:"
+
+    puts "(1) Mastermind has two players: the Codemaker and the Codebreaker"
+    puts "(2) The Codemaker makes a combination of four colors that are placed\n
+              in an order of their choosing. This combination is hidden from\n
+              the Codebreaker."
+    puts "(3) The Codebreaker must then figure out what the colors are and in\n
+              what order."
+    puts "(4) Each time the Codebreaker guesses a combination, clues are provided\n
+              about the accuracy of the prediction" 
+    puts "(5) If the Codebreaker guesses the correct color in the correct place,\n
+              a red peg is provided."
+    puts "(6) If the Codebreaker guesses a correct color, but in an incorrect \n
+              place, a white peg is provided."
+    puts "(7) The Codebreaker has 12 chances to deduce the code"
+    puts "\nPress enter to continue"
+    gets
   end
 end
